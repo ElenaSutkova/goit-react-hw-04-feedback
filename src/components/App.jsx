f71a45f3fca0717c8da9f9649557ca93d4d08e39
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2024-01-04 13:52:20 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
+import React, { useState } from 'react';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+import Statistic from './Statistic/Statistic';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = (type) => {
+    switch (type) {
+      case 'good':
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedback = () => {
+    const total = countTotalFeedback();
+    return total ? Math.round((good / total) * 100) : 0;
+  };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedback();
+  const showStatistic = totalFeedback > 0;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Please leave feedback">
+        <Feedback options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+      </Section>
+
+      {showStatistic ? (
+        <Section title="Statistic">
+          <Statistic good={good} neutral={neutral} bad={bad} total={totalFeedback} positivePercentage={positivePercentage} />
+        </Section>
+      ) : (
+          <Notification message="There is no feedback" />
+      )}
     </div>
-  );
-};
+  )
+}
+
+export default App;
